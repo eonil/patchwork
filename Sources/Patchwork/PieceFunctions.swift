8c2f0e9fe0ec11a00589c6100ddb7ebce004d8e9
eonil <pub.dev@eonil.com>
2022-02-08 14:38:39 +0900

    fix: hide piece functions

--- Sources/Patchwork/PieceFunctions.swift
+++ Sources/Patchwork/PieceFunctions.swift
@@ -1,52 +1,57 @@
 import Foundation
 import CoreGraphics
 
-public func divX(version v:AnyHashable = AlwaysDifferent(), vertical h: PieceSizingMode = .fillContainer, @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
+/// Experimental piece functions.
+/// - Design is unstable.
+/// - No ideal solution for now.
+/// - Define your own functions in your program if you need one now.
+
+internal func divX(version v:AnyHashable = AlwaysDifferent(), vertical h: PieceSizingMode = .fillContainer, @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
     Piece(sizing: PieceSizing(horizontal: .fillContainer, vertical: h), content: .stitch(Stitch(version: v, content: {
         StitchContent(axis: .horizontal, segments: c())
     })))
 }
-public func divY(version v:AnyHashable = AlwaysDifferent(), horizontal w: PieceSizingMode = .fillContainer, @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
+internal func divY(version v:AnyHashable = AlwaysDifferent(), horizontal w: PieceSizingMode = .fillContainer, @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
     Piece(sizing: PieceSizing(horizontal: w, vertical: .fillContainer), content: .stitch(Stitch(version: v, content: {
         StitchContent(axis: .vertical, segments: c())
     })))
 }
-public func stackZ(version x:AnyHashable = AlwaysDifferent(), horizontal h: PieceSizingMode = .fillContainer, vertical v: PieceSizingMode = .fillContainer, @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
+internal func stackZ(version x:AnyHashable = AlwaysDifferent(), horizontal h: PieceSizingMode = .fillContainer, vertical v: PieceSizingMode = .fillContainer, @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
     Piece(horizontal: h, vertical: v, content: .stack(Stack(version: x, content: {
         c()
     })))
 }
 
-public func fill(_ p:@escaping() -> Piece) -> Piece {
+internal func fill(_ p:@escaping() -> Piece) -> Piece {
     var z = p()
     z.sizing.horizontal = .fillContainer
     z.sizing.vertical = .fillContainer
     return z
 }
-public func fillX(_ p:@escaping() -> Piece) -> Piece {
+internal func fillX(_ p:@escaping() -> Piece) -> Piece {
     var z = p()
     z.sizing.horizontal = .fillContainer
     return z
 }
-public func fillY(_ p:@escaping() -> Piece) -> Piece {
+internal func fillY(_ p:@escaping() -> Piece) -> Piece {
     var z = p()
     z.sizing.vertical = .fillContainer
     return z
 }
 
 /// Makes contained piece as fit-content mode.
-public func fit(_ p:@escaping() -> Piece) -> Piece {
+internal func fit(_ p:@escaping() -> Piece) -> Piece {
     var z = p()
     z.sizing.horizontal = .fitContent
     z.sizing.vertical = .fitContent
     return z
 }
-public func fitX(_ p:@escaping() -> Piece) -> Piece {
+internal func fitX(_ p:@escaping() -> Piece) -> Piece {
     var z = p()
     z.sizing.horizontal = .fitContent
     return z
 }
-public func fitY(_ p:@escaping() -> Piece) -> Piece {
+internal func fitY(_ p:@escaping() -> Piece) -> Piece {
     var z = p()
     z.sizing.vertical = .fitContent
     return z
@@ -54,45 +59,45 @@ public func fitY(_ p:@escaping() -> Piece) -> Piece {
 
 /// It's unclear that this function is well-designed or not.
 @available(*, deprecated)
-public func wrapX(version v:AnyHashable = AlwaysDifferent(), @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
+internal func wrapX(version v:AnyHashable = AlwaysDifferent(), @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
     Piece(sizing: .fitContent, content: .stitch(Stitch(version: v, content: {
         StitchContent(axis: .horizontal, segments: c())
     })))
 }
 /// It's unclear that this function is well-designed or not.
 @available(*, deprecated)
-public func wrapY(version v:AnyHashable = AlwaysDifferent(), @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
+internal func wrapY(version v:AnyHashable = AlwaysDifferent(), @ArrayBuilder<Piece> content c:@escaping() -> [Piece]) -> Piece {
     Piece(sizing: .fitContent, content: .stitch(Stitch(version: v, content: {
         StitchContent(axis: .vertical, segments: c())
     })))
 }
 
-public func view(_ x:OSView) -> Piece {
+internal func view(_ x:OSView) -> Piece {
     Piece(sizing: .fillContainer, content: .view(x))
 }
-public func text(_ x:NSAttributedString) -> Piece {
+internal func text(_ x:NSAttributedString) -> Piece {
     Piece(sizing: .fillContainer, content: .text(x))
 }
-public func image(_ x:OSImage) -> Piece {
+internal func image(_ x:OSImage) -> Piece {
     Piece(sizing: .fillContainer, content: .image(x))
 }
 /// Flex sized color.
-public func color(_ x:OSColor) -> Piece {
+internal func color(_ x:OSColor) -> Piece {
     Piece(sizing: .fillContainer, content: .color(ColorPieceContent(size: .zero, color: x)))
 }
 /// Flex sized space.
-public func space() -> Piece {
+internal func space() -> Piece {
     Piece(sizing: .fillContainer, content: .space(.zero))
 }
 
 
 
 /// Rigid sized color.
-public func fitColor(_ x:OSColor, width w:CGFloat, height h:CGFloat) -> Piece {
+internal func fitColor(_ x:OSColor, width w:CGFloat, height h:CGFloat) -> Piece {
     Piece(sizing: .fitContent, content: .color(ColorPieceContent(size: CGSize(width: w, height: h), color: x)))
 }
 /// Rigid sized space.
-public func fitSpace(width w:CGFloat, height h:CGFloat) -> Piece {
+internal func fitSpace(width w:CGFloat, height h:CGFloat) -> Piece {
     Piece(sizing: .fitContent, content: .space(CGSize(width: w, height: h)))
 }
 
